a80516a9cb3f28a5a761b027bbfdd088843ddf56
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-05-28 22:31:31 +0100

    Initial Commit

---
+++ BarUnderneath/BarUnderneathApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarUnderneathApp.swift
+//  BarUnderneath
+//
+//  Created by Miaomiao Shi on 28/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarUnderneathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarUnderneath/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BarUnderneath
+//
+//  Created by Miaomiao Shi on 28/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
